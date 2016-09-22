fc5ce005f616c3ca02fb051fa34d9427b6409d2f
Salman Rameli <salmanrameli@hotmail.com>
2016-09-22 11:00:29 +0700

    merubah tipe data array angka, variabel kiri, variabel pindah dari int jadi float

--- source.cpp
+++ source.cpp
@@ -3,9 +3,9 @@
 int main()
 {
 	int i, j, k; //variabel looping
-	int angka[3]; //array menyimpan angka persamaan
-	int kiri; //variabel menyimpan angka yang dipindah ke kiri persamaan
-	int pindah; //variabel menyimpan angka yang dipilih untuk dipindah ke kiri
+	float angka[3]; //array menyimpan angka persamaan
+	float kiri; //variabel menyimpan angka yang dipindah ke kiri persamaan
+	float pindah; //variabel menyimpan angka yang dipilih untuk dipindah ke kiri
 	//char operatr[2]; //array menyimpan operator
 	int x0; //variabel menyimpan nilai X0
 	int sebenarnya; //variabel menyimpan kunci jawaban
@@ -13,7 +13,7 @@ int main()
 	printf("masukkan angka-angka persamaan: ");
 	for(i=0;i<3;i++)
 	{
-		scanf("%d", &angka[i]);
+		scanf("%f", &angka[i]);
 	}
 	
 	printf("masukkan nilai X0: ");
@@ -22,26 +22,27 @@ int main()
 	printf("masukkan nilai sebenarnya: ");
 	scanf("%d", &sebenarnya);
 	
-	printf("persamaan: %dX^2", angka[0]);
-	if(angka[1] < 0)
-	{
-		printf(" - %dX", angka[1]*-1);
-	}
-	if(angka[1] > 0)
-	{
-		printf(" + %dX", angka[1]);
-	}
-	if(angka[2] < 0)
-	{
-		printf(" - %d\n", angka[2]*-1);
-	}
-	if(angka[2] > 0)
-	{
-		printf(" + %d\n", angka[2]);
-	}
+	//untuk debugging
+//	printf("persamaan: %dX^2", angka[0]);
+//	if(angka[1] < 0)
+//	{
+//		printf(" - %dX", angka[1]*-1);
+//	}
+//	if(angka[1] > 0)
+//	{
+//		printf(" + %dX", angka[1]);
+//	}
+//	if(angka[2] < 0)
+//	{
+//		printf(" - %d\n", angka[2]*-1);
+//	}
+//	if(angka[2] > 0)
+//	{
+//		printf(" + %d\n", angka[2]);
+//	}
 	
-	printf("masukkan angka yang dipindahkan ke kiri: ");
-	scanf("%d", &pindah);
+//	printf("masukkan angka yang dipindahkan ke kiri: ");
+//	scanf("%f", &pindah);
 	
 	//jika angka yang dipindahkan berada di indeks ke-0 array
 //	if(pindah == angka[0])
@@ -53,21 +54,22 @@ int main()
 //		angka[1] = angka[2];
 //		angka[2] = 0;
 //	}
-	if(pindah == angka[1]) //jika angka yang dipindahkan berada di indeks ke-1 array
-	{
-		kiri = angka[1]*-1;
+
+	kiri = angka[1]*-1;
 		
-		//indeks array setelah indeks ke-1 digeser 1 ke kiri
-		angka[1] = angka[2];
-		angka[2] = 0;
-	}
+	//indeks array setelah indeks ke-1 digeser 1 ke kiri
+	angka[1] = angka[2];
+	angka[2] = 0;
 	
-	printf("angka di kiri: %d\n", kiri);
+	//untuk debugging
+	printf("angka di kiri: %f\n", kiri);
 	printf("persamaan sekarang: ");
 	for(i=0;i<2;i++)
 	{
-		printf("%d", angka[i]);
+		printf("%f", angka[i]);
 	}
+
+	
 	
 	return 0;
 }
