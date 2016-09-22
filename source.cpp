e041367b37e31d0411949848eddd43853ae78a92
Salman Rameli <salmanrameli@hotmail.com>
2016-09-22 09:57:41 +0700

    menambahkan variabel2 dan meminta input dari user

--- source.cpp
+++ source.cpp
@@ -2,5 +2,23 @@
 
 int main()
 {
+	int i, j, k; //variabel looping
+	int angka[3]; //array menyimpan angka persamaan
+	char operatr[2]; //array menyimpan operator
+	int x0; //variabel menyimpan nilai X0
+	int sebenarnya; //variabel menyimpan kunci jawaban
 	
+	printf("masukkan angka-angka persamaan: ");
+	for(i=0;i<3;i++)
+	{
+		scanf("%d", &angka[i]);
+	}
+	
+	printf("masukkan nilai X0: ");
+	scanf("%d", &x0);
+	
+	printf("masukkan nilai sebenarnya: ");
+	scanf("%d", &sebenarnya);
+	
+	return 0;
 }
